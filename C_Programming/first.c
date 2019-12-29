29d7efa9d0b6c46730257e11d6b578865a21d848
Rahul Pathak <rahulzha93@gmail.com>
2019-12-29 16:31:16 +0545

    c program

---
+++ C_Programming/first.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+// left to right ==> single line comment
+/*
+kjdjbcjksdbv
+sddkvvnsjdv
+multi line comment
+*/
+void main()
+{
+    //pintf("Hello World!");
+    //printf("Welcome              to \nC\tProgramming!");
+    printf("   *\n  ***\n *****\n*******");
+}
+
+
+
+
+
+
+
+
+
---
+++ C_Programming/variables.c
@@ -0,0 +1,34 @@
+/* variables in c
+Add two numbers
+*/
+#include<stdio.h>
+// escape sequence , format specifier, data types, operators
+// conditionals
+int main()
+{
+    int x,y;
+   //int x,y,sum;
+   //x = 2198;
+   //y = 3349;
+   //printf("Enter First Number : ");
+   //scanf("%d",&x);
+   //printf("Enter Second Number : ");
+   //scanf("%d",&y);
+   printf("Enter two numbers : ");
+   scanf("%d%d",&x,&y);
+   // & ==> address of
+   //sum = x + y;
+
+   //printf("the sum is %d %d",sum,x);
+   printf("%d + %d = %d",x,y,(x+y));
+
+   return 10;
+}
+/*
+2 + 3 = 5
+x + y = sum
+*/
+
+
+
+
