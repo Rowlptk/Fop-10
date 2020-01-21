21a3487c75b72029b978f1ea4c6ae2d5fab380c2
Rahul Pathak <rahulzha93@gmail.com>
2020-01-21 15:27:06 +0545

    c app init

--- C_Programming/swissBank.c
+++ C_Programming/swissBank.c
@@ -3,10 +3,18 @@
 
 void login();
 void menu();
+void checkbalance();
+
+float total = 1000;
 
 void main()
 {
     login();
+}
+
+void checkbalance()
+{
+    printf("Your Total Balance is %.2f",total);
 }
 
 void menu()
@@ -15,6 +23,24 @@ void menu()
     printf("************* Welcome to Swiss Bank ***************\n");
     printf("Options:\n1. Check Balance\n2. Withdraw\n3. Deposit\n4. Exit\n   Select : ");
     scanf("%d",&n);
+    switch(n)
+    {
+    case 1:
+        //printf("Check Balance");
+        checkbalance();
+        break;
+    case 2:
+        printf("Withdraw");
+        break;
+    case 3:
+        printf("Deposit");
+        break;
+    case 4:
+        printf("Exit");
+        break;
+    default:
+        printf("Invalid Option!");
+    }
 }
 
 
@@ -28,7 +54,7 @@ void login()
     system("cls");
     if((strcmp(usr,"admin")==0)&&(strcmp(pwd,"admin@123")==0))
     {
-        printf("\nWelcome %s\n",usr);
+        printf("Welcome %s\n\n",usr);
         menu();
     }
     else
