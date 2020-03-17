2fe29ff6774984b688f962491f90a27ca34a06b1
Rahul Pathak <rahulzha93@gmail.com>
2020-03-17 16:37:51 +0545

    17Mar2020

---
+++ sql/SQLQuery1.sql
@@ -0,0 +1,23 @@
+-- single line comment
+
+/* multi
+line
+comment*/
+
+-- create database
+create database qwerty1
+
+use qwerty1
+
+create table x_men
+(
+	id int,
+	name varchar(50)
+)
+
+select * from x_men
+
+insert into x_men values(101,'Wolverine'),
+(102,'Magneto'),(103,'Charles'),(104,'Rogue')
+
+drop database qwerty1
